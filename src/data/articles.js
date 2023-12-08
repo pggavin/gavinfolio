f03102011d3e118c45397f174bd45e25a4128420
pggavin <122394484+pggavin@users.noreply.github.com>
2023-12-07 19:51:28 -0800

    COOL STUFF

--- src/data/articles.js
+++ src/data/articles.js
@@ -60,6 +60,48 @@ function article_2() {
 	};
 }
 
-const myArticles = [article_1, article_2];
+function article_3() {
+	return {
+		date: "7 May 2023",
+		title: "RoboStrike 3.0",
+		description:
+			"AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
+		style: ``,
+		keywords: [
+			"Artificial Intelligence in Healthcare",
+			"Tharindu",
+			"Tharindu N",
+			"Tharindu Nayanajith",
+		],
+		body: (
+			<React.Fragment>
+				<h1>Content of article 2</h1>
+			</React.Fragment>
+		),
+	};
+}
+
+function article_4() {
+	return {
+		date: "7 May 2023",
+		title: "Penguin Rush",
+		description:
+			"AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
+		style: ``,
+		keywords: [
+			"Artificial Intelligence in Healthcare",
+			"Tharindu",
+			"Tharindu N",
+			"Tharindu Nayanajith",
+		],
+		body: (
+			<React.Fragment>
+				<h1>Content of article 2</h1>
+			</React.Fragment>
+		),
+	};
+}
+
+const myArticles = [article_1, article_2, article_3, article_4];
 
 export default myArticles;
